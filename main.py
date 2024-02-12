102a80b3cda583e8ef6de8080451577ccb030553
Prisca C <prisca.castanier@gmail.com>
2024-02-12 17:02:37 +0100

    reformat file

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import time
 import event
 from dotenv import load_dotenv, dotenv_values
 
+from state_handler.agent import SpecialAgentState
+
 load_dotenv()
 
 config = dotenv_values(".env")
+print(config)
 
 PLAYER_ID = config.get("PLAYER_ID")
-print(PLAYER_ID)
 ARENA = config.get("ARENA")
 USERNAME = config.get("USERNAME")
 PASSWORD = config.get("PASSWORD")
@@ -29,51 +31,53 @@ state = 'monitor'
 
 
 def moveTo(agentToMove: pytactx.Agent, x: int, y: int):
-  while not (agentToMove.x == x and agentToMove.y == y):
-    agentToMove.moveTowards(x, y)
-    agentToMove.update()
+    while not (agentToMove.x == x and agentToMove.y == y):
+        agentToMove.moveTowards(x, y)
+        agentToMove.update()
 
 
 def updateColor(agentToUpdate: pytactx.Agent):
-  if state == 'monitor':
-    agent.setColor(0, 255, 0)
-  elif state == 'search':
-    agentToUpdate.setColor(255, 90, 0)
-  elif state == 'attack':
-    agentToUpdate.setColor(255, 0, 0)
-  agentToUpdate.update()
+    if state == 'monitor':
+        agent.setColor(0, 255, 0)
+    elif state == 'search':
+        agentToUpdate.setColor(255, 90, 0)
+    elif state == 'attack':
+        agentToUpdate.setColor(255, 0, 0)
+    agentToUpdate.update()
 
 
 def checkpointMonitoring(agentToMove: pytactx.Agent):
-  global state
-  while state == 'monitor' and agentToMove.distance == 0:
-    agentToMove.fire(False)
-    for i in range(len(cp)):
-      moveTo(agentToMove, cp[i][0], cp[i][1])
-      time.sleep(0.3)
+    global state
+    while state == 'monitor' and agentToMove.distance == 0:
+        agentToMove.fire(False)
+        for i in range(len(cp)):
+            moveTo(agentToMove, cp[i][0], cp[i][1])
+            time.sleep(0.3)
 
-  state = 'search'
+    state = 'search'
 
 
 def searchForEnnemy(agentToMove: pytactx.Agent):
-  global state
-  while state == 'search':
-    if agentToMove.distance == 0:
-      state = 'monitor'
-      break
+    global state
+    while state == 'search':
+        if agentToMove.distance == 0:
+            state = 'monitor'
+            break
 
 
 def attack(agentToMove: pytactx.Agent):
-  pass
+    pass
 
 
-while True:
+# while True:
+#
+#     updateColor(agent)
+#     if state == 'monitor':
+#         checkpointMonitoring(agent)
+#     elif state == 'search':
+#         searchForEnnemy(agent)
+#     elif state == 'attack':
+#         attack(agent)
+#     time.sleep(0.1)
 
-  updateColor(agent)
-  if state == 'monitor':
-    checkpointMonitoring(agent)
-  elif state == 'search':
-    searchForEnnemy(agent)
-  elif state == 'attack':
-    attack(agent)
-  time.sleep(0.1)
+SpecialAgentState(agent)
