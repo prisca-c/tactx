effab996d53d32d50f303b6727ac9416e89ae7a7
Prisca <prisca.castanier@gmail.com>
2024-02-12 22:37:39 +0100

    feat(actions): add actions to perform inside states and agent's methods

--- state_handler/agent.py
+++ state_handler/agent.py
@@ -1,42 +1,83 @@
-from state_handler.state import StateMachine, State
+import time
+
+from state_handler.state import StateMachine, IState
 import j2l.pytactx.agent as pytactx_agent
 
 
 class AgentState:
     def __init__(self, _agent: pytactx_agent.Agent):
         super().__init__()
-        self.__agent = _agent
+        self._agent = _agent
 
-    @property
-    def agent(self):
-        return self.__agent
+    def fire(self, enable: bool = True, firepath: any = None):
+        return self._agent.fire(enable, firepath)
 
-    def _on_update(self):
-        self.agent.update()
+    def move_to(self, x: int, y: int):
+        while not (self._agent.x == x and self._agent.y == y):
+            self._agent.moveTowards(x, y)
+            self._agent.update()
+
+    def on_update(self):
+        self._agent.update()
+
+    def get_distance(self):
+        return self._agent.distance
+
+    def get_color(self):
+        return self._agent.color
+
+    def set_color(self, r: int, g: int, b: int):
+        self._agent.setColor(r, g, b)
 
 
-class SpecialAgentState(AgentState):
-    def __init__(self, player_instance: pytactx_agent.Agent):
-        super().__init__(player_instance)
+class SpecialAgentState:
+    def __init__(self, player_instance: pytactx_agent, zones_to_monitor: list[tuple]):
+        self._agent = AgentState(player_instance)
         self.__fsm = StateMachine(None)
-        self.__fsm.set_state(self)
+        self.__zones = zones_to_monitor
+        while True:
+            if self._agent.get_distance() == 0:
+                print("Zone Monitoring")
+                self._zone_monitoring()
+            else:
+                print("Attacking")
+                self._attack()
+
+    def _zone_monitoring(self) -> None:
+        scan_state = ScanState(self._agent, self.__zones)
+        self.__fsm.set_state(scan_state)
+        scan_state.do_action()
+
+    def _attack(self):
+        attack_state = AttackState(self._agent)
+        self.__fsm.set_state(attack_state)
+        attack_state.do_action()
 
 
-class ScanState(State):
-    def __init__(self, state_machine, player_instance: pytactx_agent.Agent):
-        super().__init__(state_machine)
-        self.__agent = player_instance
+class ScanState(IState):
+    def __init__(self,
+                 player_instance: AgentState,
+                 zones: list[tuple]):
+        super().__init__()
+        self.__agent: AgentState = player_instance
+        self.__zones = zones
 
     def do_action(self):
         self.scan()
 
     def scan(self):
         self.__agent.fire(False)
+        self.__agent.set_color(0, 255, 0)
+        self.__agent.on_update()
+        for i in range(len(self.__zones)):
+            self.__agent.move_to(self.__zones[i][0], self.__zones[i][1])
+            self.__agent.on_update()
+            time.sleep(0.3)
 
 
-class AttackState(State):
-    def __init__(self, state_machine, player_instance: pytactx_agent.Agent):
-        super().__init__(state_machine)
+class AttackState(IState):
+    def __init__(self, player_instance: AgentState):
+        super().__init__()
         self.__agent = player_instance
 
     def do_action(self):
@@ -44,3 +85,5 @@ class AttackState(State):
 
     def attack(self):
         self.__agent.fire(True)
+        self.__agent.set_color(255, 0, 0)
+        self.__agent.on_update()
